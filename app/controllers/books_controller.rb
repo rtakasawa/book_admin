1577d1441b8687280502b49661df9d819be4ec7e
ryosuke takasawa <ten.r0619@gmail.com>
2020-09-02 06:58:10 +0900

    95pまで

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  protect_from_forgery expect: [:destroy]
+  before_action :set_book, only: [:show, :destroy]
+
+  def show
+    render :show
+  end
+
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to "/" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+end
